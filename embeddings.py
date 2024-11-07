11f7ea6bd38ca4aa00f867f60d03f606da502ba7
Kunalvasisht <113524278+Kunalvasisht@users.noreply.github.com>
2024-11-07 12:59:46 +0530

    Update embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -1,11 +1,7 @@
 from sentence_transformers import SentenceTransformer
 
-# Load the pre-trained SentenceTransformer model
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
 def get_embedding(text: str):
-    """
-    Function to get the embedding of a text using the SentenceTransformer model.
-    """
     embedding = model.encode(text)
     return embedding
